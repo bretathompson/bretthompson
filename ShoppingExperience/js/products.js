cb70ccb6646f258f4181f8d275bde67de0a55d6d
bretathompson <68876276+bretathompson@users.noreply.github.com>
2023-09-28 14:43:29 -0600

    Add files via upload

--- ShoppingExperience/js/products.js
+++ ShoppingExperience/js/products.js
@@ -22,116 +22,62 @@ const products = [
     },
 
     {
-        id: 3,
-        image: imagePath + "SquishyCaterpillar_Thumb.jpg",
-        name: "Squishy Caterpillar",
-        description: "A squashy, spiky, multi-colored caterpillar",
-        quantity: 1,
-        price: 9.99,
-      },
+      id: 3,
+      image: imagePath + "SquishyCaterpillar_Thumb.jpg",
+      name: "Squishy Caterpillar",
+      description: "A squashy, spiky, multi-colored caterpillar",
+      quantity: 1,
+      price: 9.99,
+    },
 
-      {
-        id: 4,
-        image: imagePath + "MiniCactus_Thumb.jpg",
-        name: "Mini Cactus",
-        description: "A miniature cactus in a pink vase",
-        quantity: 1,
-        price: 12.99,
-      },
+    {
+      id: 4,
+      image: imagePath + "MiniCactus_Thumb.jpg",
+      name: "Mini Cactus",
+      description: "A miniature cactus in a pink vase",
+      quantity: 1,
+      price: 12.99,
+    },
 
-      {
-        id: 5,
-        image: imagePath + "Nanoblocks_Thumb.jpg",
-        name: "Nano Nanoblocks",
-        description: "The smallest connection blocks on the market",
-        quantity: 1,
-        price: 32.95,
-      },
+    {
+      id: 5,
+      image: imagePath + "Nanoblocks_Thumb.jpg",
+      name: "Nano Nanoblocks",
+      description: "The smallest connection blocks on the market",
+      quantity: 1,
+      price: 32.95,
+    },
 
-      {
-        id: 6,
-        image: imagePath + "CuteGoat_Thumb.jpg",
-        name: "Cute Goat",
-        description: "A small white ceramic goat",
-        quantity: 1,
-        price: 15.95,
-      },
+    {
+      id: 6,
+      image: imagePath + "CuteGoat_Thumb.jpg",
+      name: "Cute Goat",
+      description: "A small white ceramic goat",
+      quantity: 1,
+      price: 15.95,
+    },
 
-      {
-        id: 7,
-        image: imagePath + "LawnGnome_Thumb.jpg",
-        name: "Lawn Gnome",
-        description: "An angry lawn gnome holding a sign that says 'GO AWAY!",
-        quantity: 1,
-        price: 7.95,
-      },
+    {
+      id: 7,
+      image: imagePath + "LawnGnome_Thumb.jpg",
+      name: "Lawn Gnome",
+      description: "An angry lawn gnome holding a sign that says 'GO AWAY!",
+      quantity: 1,
+      price: 7.95,
+    },
 
-      {
-        id: 8,
-        image: imagePath + "BobRoss_Thumb.jpg",
-        name: "Bob Ross",
-        description: "A Bob Ross plastic figure holding a paint palette",
-        quantity: 1,
-        price: 18.95,
-      },
+    {
+      id: 8,
+      image: imagePath + "BobRoss_Thumb.jpg",
+      name: "Bob Ross",
+      description: "A Bob Ross plastic figure holding a paint palette",
+      quantity: 1,
+      price: 18.95,
+    },
     
   ];
   
 
-//   // Select the cardContainer div
-// const cardContainer = document.querySelector('#cardContainer');
-
-// // Function to add products to the page
-// function addProducts() {
-//   // Iterate over each product in the products array
-//   products.forEach((individualCard) => {
-//     // Create a card element
-//     const card = document.createElement('div');
-//     card.className = 'card';
-//     card.id = `cardNumber${individualCard.id}`;
-
-//     // Create an image element
-//     const img = document.createElement('img');
-//     img.src = individualCard.image;
-//     img.alt = individualCard.description;
-
-//     // Create a cardText div
-//     const cardText = document.createElement('div');
-//     cardText.className = 'cardText';
-
-//     // Create an h4 element for product name
-//     const productName = document.createElement('h4');
-//     productName.textContent = individualCard.name;
-
-//     // Create a p element for product description
-//     const productDescription = document.createElement('p');
-//     productDescription.textContent = individualCard.description;
-
-//     // Create a p element for product price
-//     const productPrice = document.createElement('p');
-//     productPrice.textContent = `$ ${individualCard.price}`;
-
-//     // Create a button element for adding to cart
-//     const addToCartButton = document.createElement('button');
-//     addToCartButton.className = 'cartButton';
-//     addToCartButton.textContent = 'Add to Cart';
-//     addToCartButton.id = individualCard.id; // Set the id to match the product id
-
-//     // Append elements to the card
-//     card.appendChild(img);
-//     cardText.appendChild(productName);
-//     cardText.appendChild(productDescription);
-//     cardText.appendChild(productPrice);
-//     cardText.appendChild(addToCartButton);
-//     card.appendChild(cardText);
-
-//     // Append the card to the cardContainer
-//     cardContainer.appendChild(card);
-//   });
-// }
-
-// // Call the addProducts function to populate the page with products
-// addProducts();
 
 
 
--- ShoppingExperience/js/shop.js
+++ ShoppingExperience/js/shop.js
@@ -1,82 +1,25 @@
 
 
-
-
-// <div class="card" id="cardNumber1">
-//                 <img src="images/shop/thumbnails/ClownUmbrella_Thumb.jpg" alt="clownUmbrella">
-//                 <div class="cardText">
-//                     <h4>Clown Umbrella</h4>
-//                     <p>A porcelain figurine of a clown holding an umbrella and riding a unicycle</p>
-//                     <p>&dollar; 29.95</p>
-//                     <button class="cartButton" type="button">Add to Cart</button>
-//                 </div>
-//             </div>
- 
-
-  // Select the cardContainer div
 const cardContainer = document.querySelector('#cardContainer');
 
-// Function to add products to the page
 function addProducts() {
-  // Iterate over each product in the products array
-  products.forEach((individualCard) => {
-    // Create a card element
-    const card = document.createElement('div');
-    card.className = 'card';
-    card.id = `cardNumber${individualCard.id}`;
+    let cardInfo = '';
 
-    // Create an image element
-    const img = document.createElement('img');
-    img.src = individualCard.image;
-    img.alt = individualCard.description;
+    products.forEach((individualCard) => {
+      cardInfo += `
+            <div class="card" id="cardNumber${individualCard.id}">
+                <img src="${individualCard.image}" alt="${individualCard.description}">
+                <div class="cardText">
+                    <h4>${individualCard.name}</h4>
+                    <p>${individualCard.description}</p>
+                    <p>&dollar; ${individualCard.price}</p>
+                    <button class="cartButton" id="${individualCard.id}" type="button">Add to Cart</button>
+                </div>
+            </div>`;
+    });
 
-    // Create a cardText div
-    const cardText = document.createElement('div');
-    cardText.className = 'cardText';
-
-    // Create an h4 element for product name
-    const productName = document.createElement('h4');
-    productName.textContent = individualCard.name;
-
-    // Create a p element for product description
-    const productDescription = document.createElement('p');
-    productDescription.textContent = individualCard.description;
-
-    // Create a p element for product price
-    const productPrice = document.createElement('p');
-    productPrice.textContent = `$ ${individualCard.price}`;
-
-    // Create a button element for adding to cart
-    const addToCartButton = document.createElement('button');
-    addToCartButton.className = 'cartButton';
-    addToCartButton.textContent = 'Add to Cart';
-    addToCartButton.id = individualCard.id; // Set the id to match the product id
-
-    // Append elements to the card
-    card.appendChild(img);
-    cardText.appendChild(productName);
-    cardText.appendChild(productDescription);
-    cardText.appendChild(productPrice);
-    cardText.appendChild(addToCartButton);
-    card.appendChild(cardText);
-
-    // Append the card to the cardContainer
-    cardContainer.appendChild(card);
-  });
+    cardContainer.innerHTML = cardInfo;
 }
 
-// Call the addProducts function to populate the page with products
 addProducts();
-
-
-
-
-
-
-
-
-
-
-
-
 
