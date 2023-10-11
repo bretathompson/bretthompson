16ebe9ce15a8431d5a9c7a91aad47231c3a4e944
bretathompson <68876276+bretathompson@users.noreply.github.com>
2023-10-10 20:04:57 -0600

    Add files via upload

--- ch13/book-pr/pr13-2.1.js
+++ ch13/book-pr/pr13-2.1.js
@@ -1,30 +1,78 @@
 
-// function set_it() {
-//     let name = prompt("Enter your name", "");    
-//     text = "new_cookie" + encodeURIComponent(name),
-//     expdate = ";expires=Mon, 1 Jan 2024 00:00:01 UTC";
-//     text += expdate;
-//     document.cookie = text;
-// }
-
-// function read_it() {
-//     let cookie = document.cookie;
-//     propval = cookie.split(';');
+function set_it() {
+    let name = prompt("Enter your name", "");    
+    text = "new_cookie" + encodeURIComponent(name),
+    expdate = "-expires=Mon, 1 Jan 2024 00:00:01 UTC";
+    text += expdate;
+    document.cookie = 'hello';
     
-//     if (document.cookie.split(';').some(function(item) {
-//         item.trim().startsWith("username=");
-//     })) {
-//         read_it();
-//         let name = cookie.substring("username=".length);
-//         document.getElementById("greeting").innerHTML = "Welcome, " + name + "!";
+alert(document.cookie);
+read_it();
+}
+set_it();
+
+function read_it() {
+    let cookie = document.cookie;
+       alert(cookie);
+
+    // if (document.cookie.split('-').some(function(item) {
+    //     item.trim().startsWith("new_cookie=");
+    // })) {
+        let name = cookie.substring("new_cookie=".length);
+        document.getElementById("greeting").innerHTML = "Welcome, " + name + "!";
+    // } else {
+    //      set_it();
+    //     read_it();
+    // }
+}
+
+
+// https://www.w3schools.com/js/js_cookies.asp
+
+// function set_it() {
+//     let name = prompt("Enter your name", " ");
+
+//     const date = new Date();
+//     date.setTime(date.getTime() + (1*24* 60 * 60 * 1000));
+//     // let expires = "expires="+ date.toUTCString();
+//     document.cookie = "username=John Smith - expires=Thu, 18 Dec 2013 12:00:00 UTC - path=/"
+//     let decodedCookie = decodeURIComponent(document.cookie);
+
+//     let ca = decodedCookie.split('-');
+
+//     console.log(document.cookie);
+//   }
+// set_it();
+
+// function getCookie(cname) {
+//     let name = cname + "=";
+//     let decodedCookie = decodeURIComponent(document.cookie);
+//     let ca = decodedCookie.split(';');
+//     for(let i = 0; i < ca.length; i++) {
+//       let c = ca[i];
+//       while (c.charAt(0) == ' ') {
+//         c = c.substring(1);
+//       }
+//       if (c.indexOf(name) == 0) {
+//         return c.substring(name.length, c.length);
+//       }
+//     }
+//     return "";
+//   }
+  
+// function read_it() {
+//     let username = getCookie("username");
+//     if (username != "") {
+//         alert("Welcome  " + username + "!");
 //     } else {
-//         set_it();
-//         read_it();
+
+//         if (username != "" && username != null) {
+//             read_it("username", username, 365);
+//         }
 //     }
 // }
 
 
-// https://www.w3schools.com/js/js_cookies.asp
 
 // function setCookie(cname, cvalue, exdays) {
 //     const d = new Date();
@@ -62,48 +110,3 @@
 //     }
 // }
 
-
-
-
-
-// a. create a function named set_it() that will set a cookie. Set the expiration to a future date.
-// allow the viewer to enter a name in a prompt and then use the viewr's entry in the cookie. 
-// b. create a function named read_it() that will check whether the cookie exists and, if so, read 
-// the cookie and write the name on the page in the following format (replace<name> with the name read from the cookie):
-//     welcome, <name>!
-// c. if the cookie exists, call the read_it() function. if the cookie doesn't exist, call the set_it() followed by the 
-// read_it() function.
-
-
-
-
-function set_it() {
-    let name = prompt("Enter your name", "");
-    
-
-        let text = "username=" + encodeURIComponent(name);
-        let expdate = "; expires=Mon, 1 Jan 2024 00:00:01 UTC";
-        text += expdate;
-        document.cookie = text;
-    
-}
-
-function read_it() {
-    let cookie = document.cookie;
-    let propval = cookie.split(';');
-    
-    for (let i = 0; i < propval.length; i++) {
-        let item = propval[i].trim();
-        if (item.startsWith("username=")) {
-            let name = decodeURIComponent(item.substring("username=".length));
-            document.getElementById("greeting").innerHTML = "Welcome, " + name + "!";
-            return;
-        } else {
-            set_it();
-            read_it();
-        }
-    }
-  
-}
-// Call read_it to check for and display the cookie.
-read_it();
--- ch13/book-pr/pr13-2.js
+++ ch13/book-pr/pr13-2.js
@@ -27,3 +27,37 @@ if (document.cookie.split(';').some(function(item) {
     read_it();
 }
 
+
+-------------------------------------------------------------------------------------
+
+
+function set_it() {
+    let name = prompt("Enter your name", "");
+    
+
+        let text = "username=" + encodeURIComponent(name);
+        let expdate = "; expires=Mon, 1 Jan 2024 00:00:01 UTC";
+        text += expdate;
+        document.cookie = text;
+    
+}
+
+function read_it() {
+    let cookie = document.cookie;
+    let propval = cookie.split(';');
+    
+    for (let i = 0; i < propval.length; i++) {
+        let item = propval[i].trim();
+        if (item.startsWith("username=")) {
+            let name = decodeURIComponent(item.substring("username=".length));
+            document.getElementById("greeting").innerHTML = "Welcome, " + name + "!";
+            return;
+        } else {
+            set_it();
+            read_it();
+        }
+    }
+  
+}
+// Call read_it to check for and display the cookie.
+read_it();
