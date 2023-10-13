ecd2c4a605f09a9ea9c9bf5da4bce06fb6088ca8
bretathompson <68876276+bretathompson@users.noreply.github.com>
2023-10-12 18:31:28 -0600

    Add files via upload

--- ch13/book-pr/13-2/pr13-2.1.js
+++ ch13/book-pr/13-2/pr13-2.1.js
@@ -4,7 +4,7 @@ function set_it() {
     text = "new_cookie" + encodeURIComponent(name),
     expdate = ";expires=Mon, 1 Jan 2024 00:00:01 UTC";
     text += expdate;
-    document.cookie = 'text';
+    document.cookie = text;
     
 // alert(document.cookie);
 }
@@ -13,8 +13,8 @@ function read_it() {
     let cookie = document.cookie;
     //    alert(cookie);
     
-    let name = cookie.substring("new_cookie=".length);
-    document.getElementById("greeting").innerHTML = "Welcome, " + name + "!";
+    let name = cookie.substring("new_cookie=".length -1);
+    document.getElementById("greeting").innerHTML = "Welcome, " + name + " !";
 }
 
 
