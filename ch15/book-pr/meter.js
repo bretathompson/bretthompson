46ee889bd515cbb25918587e801327687a391f21
bretathompson <68876276+bretathompson@users.noreply.github.com>
2023-10-17 19:51:50 -0600

    Add files via upload

---
+++ ch15/book-pr/meter.js
@@ -0,0 +1,11 @@
+
+let my_win = window.innerWidth;
+    if (my_win >= 1000) {
+        window.location = "http://github.com";
+    } else {
+        window.location = "http://developer.mozilla.org";
+    }
+
+
+
+
