64d6ac4b01dc40f12502c22a55d176cf9279fc6f
bretathompson <68876276+bretathompson@users.noreply.github.com>
2023-10-17 20:29:40 -0600

    Add files via upload

--- ch15/book-pr/formLength.js
+++ ch15/book-pr/formLength.js
@@ -1,6 +1,6 @@
 
-for (let count=0; count <document.forms.length; count++) {
-    let formnum = count +1;
+for (let count = 0; count < document.forms.length; count++) {
+    let formnum = count + 1;
     document.write(`Form ${formnum} has ${document.forms[count].length} elements.<br>`);
 }
 
